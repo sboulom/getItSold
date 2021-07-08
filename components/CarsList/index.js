31f10852a92531edb3151a702dbdd0f65fbf4f8c
Steven Boulom <sboulom@gmail.com>
2021-07-07 23:23:44 -0700

    car list component

---
+++ components/CarsList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, ImageBackground } from 'react-native';
+
+const CarsList = (props) => {
+    return(
+        <View>
+            <Text>
+                test
+            </Text>
+
+        </View>
+    )
+}
