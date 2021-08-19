00f9e5b990893785696f072eeb025cc428f05614
sboulom <sboulom@gmail.com>
2021-08-19 11:49:52 -0700

    test

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -4,8 +4,8 @@ import CarItem from '../CarItem'
 import styles from './styles';
 import cars from './cars';
 
-const CarsList = (props) => {
-    return(
+// const CarsList = (props) => {
+//     return(
         <View style={styles.container}>
             <FlatList 
                 data= {cars}
